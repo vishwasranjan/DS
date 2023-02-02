a8538c428cddbfb4559bde2a441765a3508920d6
Vishwas <vishwas.ranjan@happiestminds.com>
2023-02-02 20:00:56 +0530

    commit

--- Recursion.java
+++ Recursion.java
@@ -21,6 +21,39 @@ public class Recursion {
         System.out.print(n+" ");
         PrintNumbersDesc(n-1,i);
     }
+    //Recursion - Paramaetrised and Function
+    //Sum of first n numbers(Paramerized)
+    public void SumOfNnumbers(int n,int sum)
+    {
+        if(n<1)
+        {
+            System.out.println(sum);
+            return;
+        }
+        SumOfNnumbers(n-1, sum+n);
+    }
+    //Sum of first n numbers (Functional)
+    public int SumOfNumbers(int n)
+    {
+        if(n==0)
+        {
+            return 0;
+        }
+        return n+SumOfNumbers(n-1);
+    }
+    
+
+    //Swapping an array
+    public void ReverseArray(int i,int j,int[] arr)
+    {
+        if(i<j){
+            int c=arr[i];
+            arr[i]=arr[j];
+            arr[j]=c;
+            ReverseArray(i+1, j-1, arr);
+        }
+        
+    }
     
     public static void main(String args[])
     {
@@ -31,6 +64,22 @@ public class Recursion {
         System.out.println();
         System.out.println("n-i");
         recursion.PrintNumbersDesc(10, 1);
+        System.out.println();
+        System.out.println("Sum of first n numbers(Parametrized)");
+        recursion.SumOfNnumbers(4, 0);
+        System.out.println();
+        System.out.println("Sum of first n numbers(Functional)");
+        System.out.println(recursion.SumOfNumbers(4));
+        System.out.println();
+        System.out.println("Swapping the array 123456789");
+        int a[]={1,2,3,4,5,6,7,8,9};
+        recursion.ReverseArray(0,a.length-1, a);
+        for(int i=0;i<a.length;i++)
+        {
+            System.out.print(a[i]);
+        }
+        
+
         
     }
 }
